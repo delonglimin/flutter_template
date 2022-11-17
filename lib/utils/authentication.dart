db95a50ac340c6094dd7300789b661aeb8977c61
xudelong <2452036924@qq.com>
2022-11-17 11:36:54 +0800

    remove useless

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter_getx_template/common/values/index.dart';
 import 'package:flutter_getx_template/utils/index.dart';
-import 'package:get/get.dart';
 
 /// 检查是否有 token
 Future<bool> isAuthenticated() async {
@@ -12,10 +11,4 @@ Future<bool> isAuthenticated() async {
 /// 删除缓存token
 Future deleteAuthentication() async {
   await LoacalStorage().remove(STORAGE_USER_PROFILE_KEY);
-}
-
-/// 重新登录
-void deleteTokenAndReLogin() async {
-  await deleteAuthentication();
-  Get.offAndToNamed('/login');
 }
